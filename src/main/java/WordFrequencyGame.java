abcdfd8e23d2293c00b272992f34140fbf6e2532
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-09 20:37:52 +0800

    [Refactor][ZULUEJA] refact: created getWordInfoListTemp as wordset

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,5 +1,10 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static java.util.Collections.frequency;
 
 public class WordFrequencyGame {
     public static final String BLANK_SPACE = "\\s+";
@@ -11,9 +16,8 @@ public class WordFrequencyGame {
 
 
         try {
-            List<WordInfo> wordInfoList = getListMap(sentence);
-            Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-            
+            List<WordInfo> wordInfoList = getWordInfoListTemp(sentence);
+
             return formatWordInfo(wordInfoList);
         } catch (Exception e) {
             return CALCULATE_ERROR;
@@ -22,26 +26,36 @@ public class WordFrequencyGame {
 
     private String formatWordInfo(List<WordInfo> wordInfoList) {
         return wordInfoList.stream()
-                .map(wordInfo -> String.format("%s %d", wordInfo.getWord(), wordInfo.getWord()))
+                .map(wordInfo -> String.format("%s %d", wordInfo.getWord(), wordInfo.getCount()))
                 .collect(Collectors.joining(NEW_LINE));
     }
 
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getWord())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getWord(), arr);
-            } else {
-                map.get(wordInfo.getWord()).add(wordInfo);
-            }
-        }
+    private List<WordInfo> getWordInfoListTemp(String sentence) {
+        List<String> wordList = Arrays.asList(sentence.split(BLANK_SPACE));
+        Set<String> wordSet = new HashSet<>(wordList);
+        return wordSet
+                .stream()
+                .map(word -> new WordInfo(word, frequency(wordList, word)))
+                .sorted((firstWord, secondWord) -> secondWord.getCount() - firstWord.getCount())
+                .collect(Collectors.toList());
 
-
-        return map;
     }
+
+//    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
+//        Map<String, List<WordInfo>> map = new HashMap<>();
+//        for (WordInfo wordInfo : wordInfoList) {
+////       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
+//            if (!map.containsKey(wordInfo.getWord())) {
+//                ArrayList arr = new ArrayList<>();
+//                arr.add(wordInfo);
+//                map.put(wordInfo.getWord(), arr);
+//            } else {
+//                map.get(wordInfo.getWord()).add(wordInfo);
+//            }
+//        }
+//
+//        return map;
+//    }
 
 
 }
